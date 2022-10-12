16789acf8f98a45adb988977817b055e64f4ad75
rachel <rchlmd83@gmail.com>
2022-10-12 15:20:15 +0200

    commit this

---
+++ src/main/java/no/rachelmh/leapYear/LeapYear.java
@@ -0,0 +1,21 @@
+package no.rachelmh.leapYear;
+
+public class LeapYear {
+    public boolean isLeapYear(int year) {
+        if (year % 400 == 0)
+            return true;
+
+        if (year % 100 == 0)
+            return false;
+
+        if (year % 4 == 0)
+            return true;
+
+        else
+            return false;
+
+
+
+    }
+}
+
---
+++ src/test/java/no/rachelmh/leapYear/LeapYearTest.java
@@ -0,0 +1,63 @@
+package no.rachelmh.leapYear;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeapYearTest {
+
+    LeapYear leapYear;
+    @BeforeEach
+    void setup() {
+        leapYear = new LeapYear();
+    }
+
+    @Test()
+    public void isDivisibleBy4ButNotDivisibleBy100() {
+        boolean ans = true;
+        boolean val;
+        val = leapYear.isLeapYear(2012);
+        assertEquals(ans,val);
+    }
+
+    @Test()
+    public void DivisibleBy4() {
+        boolean ans = true;
+        boolean val;
+        val = leapYear.isLeapYear(2008);
+        assertEquals(ans,val);
+    }
+
+    @Test()
+    public void DivisibleBy400() {
+        boolean ans = true;
+        boolean val;
+        val = leapYear.isLeapYear(1924);
+        assertEquals(ans, val);
+    }
+
+    @Test()
+    public void notDivisibleBy100() {
+        boolean ans = true;
+        boolean val;
+        val = leapYear.isLeapYear(1924);
+        assertEquals(ans, val);
+    }
+
+    @Test
+    public void notDivisibleBy4divisibleBy100(){
+        boolean ans = false;
+        boolean val;
+        val = leapYear.isLeapYear(1925);
+        assertEquals(ans, val);
+    }
+    @Test
+    void leapYear2004() {
+        assertTrue(leapYear.isLeapYear(2004));}
+    
+    @Test
+    void divisibleBy100butNot400(){
+        assertEquals(false, leapYear.isLeapYear(1900));
+    }
+
+}
