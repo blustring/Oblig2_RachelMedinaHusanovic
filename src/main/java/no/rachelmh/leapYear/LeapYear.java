0d7a89867ddb67edf7e1ecc159d87d0f93942e21
rachel <rchlmd83@gmail.com>
2022-10-18 13:50:57 +0200

    adding some codes

--- src/main/java/no/rachelmh/leapYear/LeapYear.java
+++ src/main/java/no/rachelmh/leapYear/LeapYear.java
@@ -1,21 +1,29 @@
 package no.rachelmh.leapYear;
 
 public class LeapYear {
+
     public boolean isLeapYear(int year) {
-        if (year % 400 == 0)
-            return true;
+        {
+            // If a year is multiple of 400, then it is a leap year
+            if (year % 400 == 0)
+                return true;
 
-        if (year % 100 == 0)
-            return false;
+            // Else If a year is multiple of 100, then it is not a leap year
+            else if (year % 100 == 0)
+                return false;
 
-        if (year % 4 == 0)
-            return true;
+            // Else If a year is multiple of 4, then it is a leap year
+            else if (year % 4 == 0)
+                return true;
 
-        else
-            return false;
+            else
+                return false;
+        }
 
+}
 
-
-    }
+   public void checker(){
+        System.out.println("okay");
+   }
 }
 
---
+++ src/main/java/no/rachelmh/leapYear/Main.java
@@ -0,0 +1,20 @@
+package no.rachelmh.leapYear;
+
+import no.rachelmh.leapYear.LeapYear;
+
+public class Main {
+
+    public static void main(String[] args){
+
+
+        System.out.println("test");
+
+        LeapYear checkLeap = new LeapYear();
+        System.out.println(checkLeap.isLeapYear(2000));
+
+        LeapYear check = new LeapYear();
+        check.checker();
+
+
+    }
+}
